12d8898d34c5235fa0519d942022d007d0bd8149
Jack-OSullivan-CS202-1001 <jacko@nevada.unr.edu>
2022-03-30 19:33:17 -0700

    Swaped namespace to std::

--- Account.h
+++ Account.h
@@ -2,9 +2,8 @@
 #define Animal_H
 
 #include <iostream>
+#include <vector>
 #include "Money.h"
-
-using namespace std;
 
 class Account{
 
@@ -15,33 +14,48 @@ class Account{
 public:
 
     Account(int dollars, int cents){
-        cout << "Account Details" << endl;
-        cout << "--------------------------" << endl;
-        cout << "Current Balance:$" << dollars << "." << cents << endl;
-        cout << "--------------------------" << endl;
+
+        std::cout << "Account Details" << std::endl;
+        std::cout << "--------------------------" << std::endl;
+        std::cout << "Current Balance:$" << dollars << "." << cents << std::endl;
+        std::cout << "--------------------------" << std::endl;
+
+        std::cout << "Account Details" << std::endl;
+        std::cout << "--------------------------" << std::endl;
+        std::cout << "Current Balance:$" << dollars << "." << cents << std::endl;
+        std::cout << "--------------------------" << std::endl;
+
         //makeDeposit;
         //makeWithdrawls;
 
     }
 
     void makeDeposit(){
+        std::cout << "--------------------" << std::endl;
+        std::cout << "Number of Deposits: "/*depositNum*/ << std::endl;
+        std::cout << "--------------------" << std::endl;
         cout << "--------------------" << endl;
         cout << "Number of Deposits: "/*depositNum*/ << endl;
         cout << "--------------------" << endl;
 
         for(int i = 0; i < //depositNum; i++ ){
-            cout << "(" << i << ") " << "$" << /*Here would be the depsoit.*/ << endl;
-        }
-        cout << "--------------------" endl;
+                       std::cout << "(" << i << ") " << "$" << /*Here would be the depsoit.*/ << std::endl;
+    }
+    std::cout << "--------------------" endl;
+
+    for(int i = 0; i < /*depositNum*/; i++ ){
+        std::cout << "(" << i << ") " << "$" << /*Here would be the depsoit.*/ << std::endl;
+    }
+    std::cout << "--------------------" std::endl;
 
     }
 
     void makeWithdrawls(){
-        cout << "Number of Withdrawals: " << /*withdrawlNum*/ << endl;
-        cout << "--------------------------" << endl;
+        std::cout << "Number of Withdrawals: " << /*withdrawlNum*/ << std::endl;
+        std::cout << "--------------------------" << std::endl;
 
         for(int i = 0; i < /*withdrawlNum; i++ */){
-                       cout << "(" << i << ") " << "$" << /*Here would be the withdral*/ << endl;
+            std::cout << "(" << i << ") " << "$" << /*Here would be the withdral*/ << std::endl;
     }
 
 };
--- Money.h
+++ Money.h
@@ -12,12 +12,9 @@ class Money{
 
 public:
     
-<<<<<<< HEAD
-    Money(int dollars, int cents){dollars = dollar; cents = cents;}
-=======
+    Money(int dollars, int cents){dollars = dollars; cents = cents;}
     Money(){}
     Money(int dollars, int cents){dollars = dollars; cents = cents;}
->>>>>>> 1215abe792eae08e0d8b6649b3eb97e0bce9d25f
 
 };
 
